8efe3143c7b59ab5385503716e6407547fc0be0a
typist <1003659191@qq.com>
2026-04-27 23:53:12 +0800

    task 2.2: 添加文本提取 API 函数

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,6 @@ export * from "./client.ts";
 export * from "./config.ts";
 export * from "./ocr.ts";
 export * from "./polling.ts";
+export * from "./text-extraction.ts";
 export * from "./types.ts";
 export * from "./upload.ts";
